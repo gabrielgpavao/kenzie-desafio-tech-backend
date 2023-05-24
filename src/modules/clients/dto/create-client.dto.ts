7306e1a6c11d491ce601d1406272ec7a51eb8016
Gabriel Pavão <gabpavao@outlook.com>
2023-05-24 13:48:21 -0300

    feat: create Client entity and dto

--- src/modules/clients/dto/create-client.dto.ts
+++ src/modules/clients/dto/create-client.dto.ts
@@ -1,1 +1,18 @@
-export class CreateClientDto {}
+import { IsString, IsEmpty, IsEmail, MaxLength } from 'class-validator'
+
+export class CreateClientDto {
+	@IsString()
+	@IsEmpty()
+	@MaxLength(127)
+		fullName: string
+
+	@IsEmail()
+	@IsEmpty()
+	@MaxLength(127)
+		email: string
+
+	@IsString()
+	@IsEmpty()
+	@MaxLength(11)
+		phoneNumber: string
+}
