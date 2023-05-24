35bcbedd4db6326d14820d4ec5286705bc78c99b
Gabriel Pavão <gabpavao@outlook.com>
2023-05-24 20:06:08 -0300

    feat: create ContactPrismaRepository and implement ContactRepository

--- src/modules/clients/repositories/client.repository.ts
+++ src/modules/clients/repositories/client.repository.ts
@@ -1,6 +1,6 @@
-import { Client } from '@prisma/client'
 import { CreateClientDto } from '../dto/create-client.dto'
 import { UpdateClientDto } from '../dto/update-client.dto'
+import { Client } from '../entities/client.entity'
 
 export abstract class ClientRepository {
 	abstract create(data: CreateClientDto): Promise<Client>
--- src/modules/contacts/repositories/contact.repository.ts
+++ src/modules/contacts/repositories/contact.repository.ts
@@ -1,11 +1,11 @@
-import { Contact } from '@prisma/client'
 import { CreateContactDto } from '../dto/create-contact.dto'
 import { UpdateContactDto } from '../dto/update-contact.dto'
+import { Contact } from '../entities/contact.entity'
 
 export abstract class ContactRepository {
-	abstract create(data: CreateContactDto): Promise<Contact>
-	abstract list(): Promise<Contact[]>;
-	abstract retrieve(id: number): Promise<Contact>;
+	abstract create(data: CreateContactDto, clientId: number): Promise<Contact>
+	abstract list(clientId: number): Promise<Contact[]>;
+	abstract retrieve(id: number, clientId: number): Promise<Contact>;
 	abstract update(id: number, data: UpdateContactDto): Promise<Contact>;
 	abstract delete(id: number): Promise<void>;
 }
