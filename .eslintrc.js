df313a348c6186121eaf45ff60c8a584f40d8995
Gabriel Pavão <gabpavao@outlook.com>
2023-05-24 11:58:34 -0300

    feat: Init Nest.js App

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'no-undef': 'off',
+		'eol-last': [
+			'error',
+			'always'
+		],
+		'no-trailing-spaces': 'error',
+		'comma-dangle': [
+			'error',
+			'never'
+		]
+	}
+}
