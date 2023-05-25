8bdc1cad42a7112714a6684642c5cebced44ad91
Gabriel Pavão <gabpavao@outlook.com>
2023-05-24 21:18:39 -0300

    feat: add password column to Client model and hash it with bcrypt

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 import 'dotenv/config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	const PORT = process.env.APP_PORT || 3001
+	app.useGlobalPipes(
+		new ValidationPipe({ whitelist: true }),
+		new ValidationPipe({
+			transform: true,
+			transformOptions: { groups: ['transform'] }
+		})
+	)
 
+	const PORT = process.env.APP_PORT || 3001
 	await app.listen(PORT, () => {
 		console.log(`Server connected and running on port ${PORT}!`)
 	})
--- src/modules/clients/entities/client.entity.ts
+++ src/modules/clients/entities/client.entity.ts
@@ -1,7 +1,13 @@
+import { Exclude } from 'class-transformer'
+
 export class Client {
 	readonly id: number
 	fullName: string
 	email: string
+
+	@Exclude()
+		password: string
+
 	phoneNumber: string
 	readonly createdAt: Date
 }
