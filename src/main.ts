91dd3d6482c79915d86015d7c5ff914e14117918
Gabriel Pavão <gabpavao@outlook.com>
2023-05-29 11:08:14 -0300

    feat: swagger initial set up

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	const config = new DocumentBuilder()
+		.setTitle('Desafio Tech')
+		.setDescription('Documentação da API criada para o Backend do Desafio Kenze Tech Full Stack.')
+		.setVersion('1.0')
+		.addTag('Clients')
+		.addBearerAuth()
+		.addTag('Contacts')
+		.addBearerAuth()
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, document)
+
 	app.enableCors({
 		origin: [
 			'http://localhost:5173',
